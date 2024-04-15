9bd069cdd9cd15cd1cbaa7ffa49178d3c8c15c4a
Piotr Żuławnik <piotr.zulawnik14@gmail.com>
2024-04-15 20:07:11 +0200

    zad3

--- controllers/books.js
+++ controllers/books.js
@@ -7,7 +7,24 @@ let Book=[
     {id:1,title:"tytul1",publishingYear:2000,authorId:1}
 ];
 
-exports.getBooksList = (req, res) => {
+const getBooksList = (req, res) => {
     res.render('books', { books:books });
 };
 
+const addBook = (req, res) => {
+
+    const { title } = req.body;
+    const id = books.length > 0 ? books[books.length - 1].id + 1 : 1;
+    books.push({ id, title });
+    res.redirect('/book/list');
+};
+
+const deleteBook = (req, res) => {
+    const { id } = req.params;
+    const index = books.findIndex(book => book.id === parseInt(id));
+    if (index !== -1) {
+        books.splice(index, 1);
+    }
+    res.redirect('/book/list');
+};
+module.exports = { getBooksList, addBook, deleteBook };
--- routes/books.js
+++ routes/books.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const booksController = require('../controllers/books');
 
 router.get('/list', booksController.getBooksList);
+router.post('/add', booksController.addBook);
+router.post('/delete/:id', booksController.deleteBook);
 
 module.exports = router;
